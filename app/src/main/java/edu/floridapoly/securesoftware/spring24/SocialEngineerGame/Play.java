84880235e2794ee6d3db4ea2310a64721e9e5f5b
FStresau <sebstresau@gmail.com>
2024-04-14 20:42:07 -0400

    test

--- app/src/main/java/edu/floridapoly/securesoftware/spring24/SocialEngineerGame/Play.java
+++ app/src/main/java/edu/floridapoly/securesoftware/spring24/SocialEngineerGame/Play.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Play extends AppCompatActivity {
     private ImageButton playButtonBack;
+    private Button answerButton1, answerButton2, answerButton3, answerButton4;
+    private TextView questionLabel, correctAnswersLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,15 @@ public class Play extends AppCompatActivity {
             }
         });
 
+        // Find the buttons and labels by their IDs
+        answerButton1 = findViewById(R.id.answerButton1);
+        answerButton2 = findViewById(R.id.answerButton2);
+        answerButton3 = findViewById(R.id.answerButton3);
+        answerButton4 = findViewById(R.id.answerButton4);
+        questionLabel = findViewById(R.id.questionLabel);
+        correctAnswersLabel = findViewById(R.id.correctAnswersLabel);
+
+        // Set the initial number of correct answers to 0
+        correctAnswersLabel.setText("Correct Answers: 0");
     }
 }
